a6832febf123042c47d98556a748b489491bea7e
zhubingwen <zhubingwen@hikvision.com.cn>
2021-11-24 09:07:44 +0800

    feat: 70爬楼梯

---
+++ easy/climbStairs70.js
@@ -0,0 +1,12 @@
+function climb(n) {
+    if (n === 0) return 0
+    if (n === 1) return 1
+    if (n === 2) return 2
+    
+    return climb(n-1) + climb(n-2)
+}
+
+
+// 爬楼梯级数
+let x = 30
+console.log(climb(x));
